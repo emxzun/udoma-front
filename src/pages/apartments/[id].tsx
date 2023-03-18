81e81721b6d4c6e768f1ddae15de8a070c4016af
danya@gmail.com <choponovdaniyar@gmail.com>
2023-03-19 04:25:43 +0600

    fix

--- src/pages/apartments/[id].tsx
+++ src/pages/apartments/[id].tsx
@@ -15,7 +15,8 @@ const ApartamentPage = () => {
       <>
         <LayoutBlock>
           <div className="apartment-detail">
-            <div className="apartment-detail__title">{DATA.apartment.title}</div>
+              <div className="apartment-detail__title">{DATA.apartment.title}</div>
+              <div className="apartment-detail__price">20{DATA.apartment.price} сом</div>
             <div className="apartment-detail__district">{DATA.apartment.district}</div>
             <Image className="apartment-detail__img" src={DATA.apartment.img} alt={DATA.apartment.title}/>
             <div className="apartment-detail__description">{DATA.apartment.desc}</div>
