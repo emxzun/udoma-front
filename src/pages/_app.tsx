8e699f717f6c95e31c3aff9d67937245b45bae3e
danya@gmail.com <choponovdaniyar@gmail.com>
2023-03-19 14:32:59 +0600

    fix redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,6 @@ import {Provider} from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <Component {...pageProps} />
   )
 }
